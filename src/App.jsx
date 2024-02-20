dcb6bf4f9f17ae2c872fe6d8eb3fbb21f3fac5f8
Roshan Badarkhe <85372087+roshan-badarkhe@users.noreply.github.com>
2024-02-20 10:03:35 +0000

    redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,17 @@
-import './App.css';
+import { increment,decrement } from "./Redux/Actions";
+import { useDispatch,useSelector } from "react-redux";
+
 
 function App() {
+
+  const count=useSelector(state=>state.count)
+  const dispatch=useDispatch();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+      <button onClick={()=>{dispatch(increment())}}>Increment</button>
+       {count}
+       <button onClick={()=>{dispatch(decrement())}}>Decrement</button>
     </div>
   );
 }
